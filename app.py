3033097e9a93611b05c94b5fde05fbf01fd0153e
Mythi3 <118481823+Mythi3@users.noreply.github.com>
2023-01-12 17:03:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from flask import Flask,jsonify,request
 
 #local imports
 from api.tests import main as run_test
+from api.handle import handle as handle
 
 #varibles
-f = open("/data/data.json","f")
+f = open("data/data.json")
 total_reqs = 0
 get_req = 0
 post_req = 0
@@ -22,12 +23,17 @@ def site_map():
     for rule in app.url_map.iter_rules():
         routes.append('%s' % rule)
     
+    #remove static
+    for route in routes:
+        if route == "/static/<path:filename>":
+            routes.remove(route)
+    
     return routes
 
 @app.route("/test", methods=["GET","POST"])
 def test():
+    ua=request.headers.get('User-Agent');data = request.data.decode("utf-8").strip();handle.log(request.remote_addr,ua,"/test",request.method,data,200)
     global total_reqs;global get_req;global post_req;method=request.method;total_reqs+=1;status = 200;data = request.data.decode("utf-8").strip();
-    
     if method=="GET": get_req += 1
     if method == "POST": post_req +=1
     output = run_test("/test",status_code=status,method=method,data=request.data,total=total_reqs,get_num=get_req,post_num=post_req)
